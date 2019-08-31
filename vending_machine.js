da9695e8e788d37008c6a6a8b8fedd2461cce459
jay23cee <jayceexxiii@gmail.com>
2019-08-31 16:05:14 -0700

    change array to dictionary

--- vending_machine.js
+++ vending_machine.js
@@ -5,7 +5,7 @@
 
 var menu_html = document.getElementsByName("menu")
 var machine = []; // create an empty array
-
+var Machine= {1:["coke", 2.60], 2:["Orange Fanta", 2.60], 3:["7up", 2.75], 4:["Water", 1.50]}
 machine[1]= ["coke", 2.60]
 machine[2]= ["Orange Fanta", 2.60],
 machine[3]= [ "7up", 2.75 ],
@@ -17,20 +17,20 @@ function Payment(){
     // getting the user Selection (id name  of html) of the form.
     selection = document.getElementById("selection").value
    
-    // if Selection exist continue if statement
-    if (machine[selection] !=undefined) {
-        // getting value of this key.
-       var dis = machine[selection]
-       // getting -0 element
-       var name= dis[0].toString()
-       //getting 1 element. cost and having 2 decimal places
-        var cost = dis[1].toFixed(2)
 
 
-        //displaying an alerrt
-        alert("you selected a " +name)
-        alert("your payment is: $" + cost )
-    }
+            // getting value of this key.
+        var dis = Machine[selection]
+        // getting -0 element
+        var name= dis[0].toString()
+        //getting 1 element. cost and having 2 decimal places
+            var cost = dis[1].toFixed(2)
+
+
+            //displaying an alerrt
+            alert("you selected a " +name)
+            alert("your payment is: $" + cost )
+        
 
     alert("PAYMENT WAS RECEIVED")
    
